401fec1b984dc24aef59c7f2fe0458036a75f6f6
Zeyuan Hu <ferrishu3886@gmail.com>
2016-02-06 05:19:06 -0600

    a

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:46,filenames:["index","sample"],objects:{},objnames:{},objtypes:{},terms:{asddddddddddddddddddd:1,content:0,page:[],sampl:[]},titles:["Zeyuan&#8217;s Knowledge base","Sample Page"],titleterms:{base:0,knowledg:0,page:1,sampl:1,zeyuan:0}})
+Search.setIndex({envversion:46,filenames:["index","sample"],objects:{},objnames:{},objtypes:{},terms:{asddddddddddddddddddd:[],content:0,page:[],sampl:[],xasddddddddddddddddddd:1},titles:["Zeyuan&#8217;s Knowledge base","Sample Page"],titleterms:{base:0,knowledg:0,page:1,sampl:1,zeyuan:0}})
